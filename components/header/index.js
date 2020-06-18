eceaf8a28f5af60a28eaeacc07b802025fd630e6
Kyle Hotchkiss <108172+kylehotchkiss@users.noreply.github.com>
2020-06-18 19:45:00 +0530

    Example component-scoped SCSS

--- components/header/index.js
+++ components/header/index.js
@@ -1,9 +1,13 @@
+import styles from "./Header.module.scss";
+
 export default () => (
   <header>
     <div className="container">
       <div className="row ">
         <div className="col mb-5">
-          <h1>Site Title</h1>
+          <div className={styles.borderBottom}>
+            <h1>Site Title</h1>
+          </div>
         </div>
       </div>
     </div>
