1bc15614c05df956cb5b8007d3cbd8404e7dff93
Cameron Corda <ccorda@gmail.com>
2020-08-24 17:45:20 -0700

    adding analytics and web vitals
    refs #2

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,37 @@
+import { useEffect } from "react";
+import Router from "next/router";
 import "styles/global.scss";
+import * as gtag from "lib/gtag";
 
-// You can set sitewide <head> tags in the <Helmet> below
+export function reportWebVitals({ id, name, label, value }) {
+  let vitalEvent = {
+    category: label === "web-vital" ? "Web Vitals" : "Next.js custom metric",
+    action: name,
+    value: Math.round(name === "CLS" ? value * 1000 : value), // values must be integers
+    label: id, // id unique to current page load
+    nonInteraction: true, // avoids affecting bounce rate.
+  };
+
+  gtag.event(vitalEvent);
+}
+
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    // Google analytics:
+    // https://github.com/vercel/next.js/blob/canary/examples/with-google-analytics/pages/_app.js
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+
+    Router.events.on("routeChangeComplete", handleRouteChange);
+
+    return () => {
+      Router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, []);
+
   return (
+    // You can set sitewide <head> tags in the <Meta> component
     <>
       <Component {...pageProps} />
     </>
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,10 +1,27 @@
 import Document, { Head, Main, NextScript } from "next/document";
+import { GA_TRACKING_ID } from "lib/gtag";
 
 export default class MyDocument extends Document {
   render() {
     return (
       <html lang="en">
         <Head>
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
           <link
             rel="preconnect"
             href="https://fonts.gstatic.com/"
