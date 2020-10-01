640e4092592a1de3e232da47a34f4a310b3c6c31
Chris Herold <cmherold@gmail.com>
2020-09-30 22:41:30 -0700

    closes #21 and #22

--- website/src/components/meta/index.js
+++ website/src/components/meta/index.js
@@ -7,27 +7,70 @@ export default function Meta({ title, description, image }) {
 
   return (
     <Head>
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <meta charSet="utf-8" />
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1"
+        key="meta_viewport"
+      />
+      <meta charSet="utf-8" key="meta_charset" />
       {/* search */}
-      <title>{meta_title}</title>
-      {description && <meta name="description" content={description} />}
-      {/* og/facebook */}
-      <meta property="og:locale" content="en_US" />
-      <meta property="og:title" content={title ? title : META_TITLE} />
-      {description && <meta property="og:description" content={description} />}
-      <meta property="og:image" content={meta_image} />
-      <meta property="og:site_name" content={META_TITLE} />
-      <meta property="og:image" content={meta_image} />
-      <meta property="og:image:width" content="1200" />
-      <meta property="og:image:height" content="600" />
-      {/* twitter */}
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta property="twitter:title" content={title ? title : META_TITLE} />
+      <title key="title">{meta_title}</title>
       {description && (
-        <meta property="twitter:description" content={description} />
+        <meta name="description" content={description} key="meta_description" />
       )}
-      <meta property="twitter:image" content={meta_image} />
+      {/* og/facebook */}
+      <meta property="og:locale" content="en_US" key="meta_og_locale" />
+      <meta
+        property="og:title"
+        content={title ? title : META_TITLE}
+        key="meta_og_title"
+      />
+      {description && (
+        <meta
+          property="og:description"
+          content={description}
+          key="meta_og_description"
+        />
+      )}
+      <meta property="og:image" content={meta_image} key="meta_og_image" />
+      <meta
+        property="og:site_name"
+        content={META_TITLE}
+        key="meta_og_site_name"
+      />
+      <meta
+        property="og:image:width"
+        content="1200"
+        key="meta_og_image_width"
+      />
+      <meta
+        property="og:image:height"
+        content="600"
+        key="meta_og_image_height"
+      />
+      {/* twitter */}
+      <meta
+        name="twitter:card"
+        content="summary_large_image"
+        key="meta_twitter_card"
+      />
+      <meta
+        property="twitter:title"
+        content={title ? title : META_TITLE}
+        key="meta_twitter_title"
+      />
+      {description && (
+        <meta
+          property="twitter:description"
+          content={description}
+          key="meta_twitter_description"
+        />
+      )}
+      <meta
+        property="twitter:image"
+        content={meta_image}
+        key="meta_twitter_image"
+      />
       {/* favicons */}
       <link rel="icon" href="/cropped-favicon-32x32.png" sizes="32x32" />
       <link rel="icon" href="/cropped-favicon-192x192.png" sizes="192x192" />
