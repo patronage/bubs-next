73866226cf456b966f73bad2c4e4a4ef0f96de2b
Cameron Corda <ccorda@gmail.com>
2020-11-14 08:12:24 -0800

    Make the pageslug a catchall route, and add support for still loading a homepage if needed.

--- website/src/pages/[[...pageslug]].js
+++ website/src/pages/[[...pageslug]].js
@@ -3,7 +3,25 @@ import { getPage, getAllPagesWithSlug } from "lib/wordpress";
 import ErrorPage from "next/error";
 import PostBody from "components/post/body";
 
-export default function Page({ post, preview }) {
+export default function Page({ post, preview, isHome }) {
+  // check if homepage
+  // you can remove this if you've defined a homepage in wordpress
+  if (isHome) {
+    return (
+      <Layout title="">
+        <section className="section-padded">
+          <div className="container">
+            <div className="row">
+              <div className="col">
+                <h3>Home Page</h3>
+              </div>
+            </div>
+          </div>
+        </section>
+      </Layout>
+    );
+  }
+
   if (!post?.slug) {
     return <ErrorPage statusCode={404} />;
   }
@@ -28,7 +46,22 @@ export default function Page({ post, preview }) {
 }
 
 export async function getStaticProps({ params, preview = false, previewData }) {
-  const data = await getPage(params.pageslug, preview, previewData);
+  // if your homepage doesn't come from WP, you need this to custom render and not get a 404
+  // next doesn't let you have index.js and [[...pageslug.js]]
+  if (!params.pageslug?.length) {
+    return {
+      props: {
+        isHome: true,
+      },
+    };
+  }
+
+  let slug = "/";
+  if (params.pageslug?.length) {
+    slug += params.pageslug.join("/");
+  }
+
+  const data = await getPage(slug, preview, previewData);
 
   return {
     props: {
