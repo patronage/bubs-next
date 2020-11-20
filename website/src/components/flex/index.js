8f6a693b2a9c0d372284bbfc3b1ba268265b6444
Kyle Hotchkiss <108172+kylehotchkiss@users.noreply.github.com>
2020-11-21 03:16:08 +0530

    working implentation of padding collapsing for flex sections

--- website/src/components/flex/index.js
+++ website/src/components/flex/index.js
@@ -1,27 +1,47 @@
+import cx from 'classnames';
 import FlexHero from '../flex-hero';
 import FlexWysiwyg from '../flex-wysiwyg';
 import FlexCarousel from '../flex-carousel';
 import FlexBlockquote from '../flex-blockquote';
 
+import styles from './flex.module.scss';
+
 const Flex = ({ sections }) => {
   const renderedSections = [];
 
   for ( let i = 0; i < sections.length; i++ ) {
-    let component;
+    let component, componentName;
+    let paddingBottom = true;
     const section = sections[i];
-    const props = { key: i };
+    const nextSection = sections[i + 1];
+
+    if ( nextSection && ( nextSection.backgroundColor === section.backgroundColor ) ) {
+      paddingBottom = false;
+    }
     
     if ( typeof section.blockquote !== 'undefined' ) {
-      component = <FlexBlockquote key={i} />
+      componentName = 'blockquote';
+      component = <FlexBlockquote />
     } else if ( typeof section.wysiwygContent !== 'undefined' ) {
-      component = <FlexWysiwyg key={i} />
+      componentName = 'wysiwyg';
+      component = <FlexWysiwyg />
     } else if ( typeof section.heroHeading !== 'undefined' ) {
-      component = <FlexHero key={i} />
+      componentName = 'hero';
+      component = <FlexHero />
     } else if ( typeof section.statsCarousel !== 'undefined' ) {
-      component = <FlexCarousel key={i} />
+      componentName = 'carousel';
+      component = <FlexCarousel  />
     }
 
-    renderedSections.push( component );    
+    const classNames = cx({
+      [styles['flex-section']]: true,
+      [`flex-${componentName}`]: true,
+      [styles['flex-padding-top']]: true,
+      [styles['flex-padding-bottom']]: paddingBottom,
+      [styles[`background-${section.backgroundColor}`]]: true
+    })
+
+    renderedSections.push(<section key={i} className={classNames}>{component}</section>);    
   }
 
   return (
