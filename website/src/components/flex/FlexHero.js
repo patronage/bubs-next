610901851706d34f99c9c33a1598fd0098333851
Kyle Hotchkiss <108172+kylehotchkiss@users.noreply.github.com>
2020-11-25 01:14:06 +0530

    Working Image component in Hero

--- website/src/components/flex/FlexHero.js
+++ website/src/components/flex/FlexHero.js
@@ -1,7 +1,23 @@
+import Image from "next/image";
 import styles from "./FlexHero.module.scss";
 
-const FlexHero = () => {
-  return <div className={styles.outer}>Hero</div>;
+const FlexHero = (props) => {
+  console.log(props.heroImage.sourceUrl);
+  return (
+    <div className={styles.outer}>
+      <h3>Hero</h3>
+
+      {props.heroImage?.sourceUrl && (
+        <div className={styles["hero-image"]}>
+          <Image
+            src={props.heroImage.sourceUrl}
+            width={props.heroImage.mediaDetails.width}
+            height={props.heroImage.mediaDetails.height}
+          />
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default FlexHero;
