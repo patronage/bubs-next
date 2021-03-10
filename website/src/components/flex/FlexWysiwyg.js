5b40b7e3077e76e11d7e9d02e3baa77da59fc6ad
Cameron Corda <ccorda@gmail.com>
2021-03-09 17:04:07 -0800

    Flex sections with better class passing, no extra div.
    refs #43

--- website/src/components/flex/FlexWysiwyg.js
+++ website/src/components/flex/FlexWysiwyg.js
@@ -3,15 +3,31 @@ import PostBody from 'components/post/PostBody';
 import styles from './FlexWysiwyg.module.scss';
 
 const FlexWysiwyg = (props) => {
+  // map any custom classes to our SCSS module style object
+  let moduleClassNames = props.customClasses?.map((className) => {
+    return styles[className];
+  });
+
   return (
-    <div className={cx([styles.outer, props.customClasses])}>
+    <section
+      className={cx([
+        props.classNames,
+        moduleClassNames,
+        styles[props.flexClass],
+      ])}
+      id={props.slug}
+    >
       <div className="container">
-        {props.sectionHeading && <h2>{props.sectionHeading}</h2>}
+        {props.sectionHeading && (
+          <h2
+            dangerouslySetInnerHTML={{ __html: props.sectionHeading }}
+          ></h2>
+        )}
         {props.wysiwygContent && (
           <PostBody content={props.wysiwygContent} />
         )}
       </div>
-    </div>
+    </section>
   );
 };
 
