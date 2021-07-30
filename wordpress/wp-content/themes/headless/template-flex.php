78199d9d48929cb414a98b1da7d775059882d712
Kyle Hotchkiss <108172+kylehotchkiss@users.noreply.github.com>
2021-07-30 07:03:33 -0700

    add 200 code to template flex and add helpful comment

--- wordpress/wp-content/themes/headless/template-flex.php
+++ wordpress/wp-content/themes/headless/template-flex.php
@@ -2,6 +2,13 @@
 
 /* Template Name: Flex */
 
+// WP Engine needs this to support auto WP updates
+$user_agent = $_SERVER['HTTP_USER_AGENT'];
+if ( strpos($user_agent, 'WPEBot') ) {
+  http_response_code(200);
+  exit();
+}
+
 $redirect = headless_redirect();
 // echo $redirect;
 wp_redirect( $redirect, 307 );
