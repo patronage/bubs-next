ef7c6c6b04bbc9b342f5ca84c587983cac8b7034
Kyle Hotchkiss <108172+kylehotchkiss@users.noreply.github.com>
2021-08-04 08:13:21 -0700

    Resolve WPEngine auto-updates (#138)
    * if user agent contains WPEBot, return 200 instead of redirect on wordpress root url to fix auto updates
    
    * add 200 code to template flex and add helpful comment

--- wordpress/wp-content/themes/headless/index.php
+++ wordpress/wp-content/themes/headless/index.php
@@ -7,6 +7,15 @@
  * E.g., it puts together the home page when no home.php file exists
  */
 
+// WP Engine needs this to support auto WP updates
+$user_agent = $_SERVER['HTTP_USER_AGENT'];
+if ( strpos($user_agent, 'WPEBot') ) {
+  http_response_code(200);
+  exit();
+}
+
 $redirect = headless_redirect();
 // echo $redirect;
-wp_redirect( $redirect, 307 );
+return wp_redirect( $redirect, 307 );
+
+?>
