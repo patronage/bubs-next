83d777f126a183dc7f2bf7b5abaa9c83c627b8fe
Kyle Hotchkiss <108172+kylehotchkiss@users.noreply.github.com>
2021-08-26 12:32:54 -0700

    Cleanup purge code a little

--- website/src/pages/api/graphcdn.js
+++ website/src/pages/api/graphcdn.js
@@ -12,62 +12,52 @@
 
 import fetch from 'isomorphic-unfetch';
 
+const GRAPHCDN_PURGE_API_URL = process.env.GRAPHCDN_PURGE_API_URL;
+const GRAPHCDN_PURGE_API_TOKEN = process.env.GRAPHCDN_PURGE_API_TOKEN;
+
+async function purgeAllPosts() {
+  const response = await fetch(GRAPHCDN_PURGE_API_URL, {
+    method: 'POST', // Always POST purge mutations
+    body: JSON.stringify({ query: 'mutation { _purgeAll }' }),
+    headers: {
+      'Content-Type': 'application/json',
+      'graphcdn-token': GRAPHCDN_PURGE_API_TOKEN,
+    },
+  });
+
+  return await response.json();
+}
+
 export default async function handler(req, res) {
-  const GRAPHCDN_PURGE_API_URL = process.env.GRAPHCDN_PURGE_API_URL;
-  const GRAPHCDN_PURGE_API_TOKEN =
-    process.env.GRAPHCDN_PURGE_API_TOKEN;
-
-  async function purgeAllPosts() {
-    console.log('attempting purge');
-    const res = await fetch(GRAPHCDN_PURGE_API_URL, {
-      method: 'POST', // Always POST purge mutations
-      headers: {
-        'Content-Type': 'application/json', // and specify the Content-Type
-        'graphcdn-token': GRAPHCDN_PURGE_API_TOKEN,
-      },
-      body: JSON.stringify({ query: 'mutation { _purgeAll }' }),
-    });
-    return await res.json();
-  }
-
-  if (req.method === 'POST') {
-    if (!GRAPHCDN_PURGE_API_URL || !GRAPHCDN_PURGE_API_TOKEN) {
-      return res
-        .status(500)
-        .end('Purge Failed, Graph CDN API endpoint not defined');
-    }
-
-    // const { slug } = req.query;
-
-    // if (!slug.includes('purge')) {
-    //   return res
-    //     .status(500)
-    //     .end('Purge Failed, must specifcy valid path');
-    // }
-
-    // if (slug.includes('post-create')) {
-    //   return res
-    //     .status(200)
-    //     .end('Skipping purge, only running on updates');
-    // }
-
-    // TODO: handle
-    console.log('body', req.body);
-
-    try {
-      const response = await purgeAllPosts();
-      console.log('CDN data', response.data);
-      if (!response.data._purgeAll) {
-        throw 'CDN Failure';
-      }
-      console.log('Purge Success');
-      return res.status(200).end('Purge Success');
-    } catch (e) {
-      console.error(e);
-      return res.status(500).end('Purge Failed');
-    }
-  } else {
+  // Only allow POST requests
+  if (req.method !== 'POST') {
     res.setHeader('Allow', 'POST');
     return res.status(405).end('Method Not Allowed');
+  }
+
+  // Verify env variable presense
+  if (!GRAPHCDN_PURGE_API_URL || !GRAPHCDN_PURGE_API_TOKEN) {
+    return res
+      .status(500)
+      .end('Purge Failed, Graph CDN API endpoint not defined');
+  }
+
+  // @TODO: Process the incoming body.post_id and create a targetted purge request
+
+  try {
+    const response = await purgeAllPosts();
+
+    console.log('CDN data', response.data);
+
+    if (!response.data._purgeAll) {
+      throw 'CDN Failure';
+    }
+
+    console.log('Purge Success');
+
+    return res.status(200).end('Purge Success');
+  } catch (e) {
+    console.error(e);
+    return res.status(500).end('Purge Failed');
   }
 }
