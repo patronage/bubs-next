4cea354550c6c04905c69f625e54ff2cc860cebf
Kyle Hotchkiss <108172+kylehotchkiss@users.noreply.github.com>
2021-09-28 14:19:59 -0700

    position param for preview mode bar

--- website/src/components/PreviewModeBar.js
+++ website/src/components/PreviewModeBar.js
@@ -1,14 +1,19 @@
+import cx from 'classnames';
 import { WORDPRESS_DOMAIN } from 'lib/constants';
 import { useState, useEffect } from 'react';
 import styles from './PreviewModeBar.module.scss';
-
 
 const WORDPRESS_EDIT_URL =
   process.env.WORDPRESS_EDIT_URL ||
   `https://${WORDPRESS_DOMAIN}/wp-admin/post.php?action=edit`;
 
-export default function PreviewModeBar({ postId }) {
+export default function PreviewModeBar({ postId, position = 'top' }) {
   const [redirect, setRedirect] = useState('/api/exit-preview');
+  let positionClassName = styles['top'];
+
+  if ( position === 'bottom' ) {
+    positionClassName = styles['bottom'];
+  }
 
   useEffect(() => {
     if (typeof location !== 'undefined') {
@@ -21,7 +26,7 @@ export default function PreviewModeBar({ postId }) {
   }, []);
 
   return (
-    <section className={styles.bar}>
+    <section className={cx(styles.bar, positionClassName)}>
       <div className="container">
         <div className="row">
           <div className="col text-center">
