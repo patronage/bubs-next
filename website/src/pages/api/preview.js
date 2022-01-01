2b299d0642e11c2ddd9394b006eca0721483578f
Cameron Corda <ccorda@gmail.com>
2022-01-01 14:39:40 -0500

    more preview debugging

--- website/src/pages/api/preview.js
+++ website/src/pages/api/preview.js
@@ -45,6 +45,8 @@ export default async function preview(req, res) {
     accessToken,
   );
 
+  console.log('preview post', post);
+
   // If the post doesn't exist prevent preview mode from being enabled
   if (!post) {
     return res.status(401).json({ message: 'Post not found' });
