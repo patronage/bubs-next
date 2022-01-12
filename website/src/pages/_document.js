aecded91cbd0c9ada1a169ccc3daa0860e909408
Cameron Corda <ccorda@gmail.com>
2022-01-11 19:46:15 -0800

    refactor: remove analytics
    closes #83

--- website/src/pages/_document.js
+++ website/src/pages/_document.js
@@ -1,4 +1,3 @@
-import { GA_TRACKING_ID } from 'lib/gtag';
 import Document, {
   Html,
   Head,
@@ -11,33 +10,6 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-
-                gtag('config', '${GA_TRACKING_ID}', {
-                  page_path: window.location.pathname,
-                });
-              `,
-            }}
-          />
-          <link
-            rel="preconnect"
-            href="https://www.googletagmanager.com"
-            crossOrigin="true"
-          />
-          <link
-            rel="preconnect"
-            href="https://www.google-analytics.com"
-            crossOrigin="true"
-          />
           <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@400;700&family=Roboto+Slab:wght@300&family=Source+Sans+Pro:wght@400;700&display=swap"
