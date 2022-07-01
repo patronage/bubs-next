e4aa6711276f9f5f38998434ea9e81443a77fb6a
Cameron Corda <ccorda@gmail.com>
2022-07-01 10:02:23 -0400

    Sitemaps robots (#222)
    * add robots and restore sitemap proxy
    
    * clean url to prevent double slash
    
    * slight docs update
    
    * add sitemap reference and disallow of api endpoints in robots file

--- website/src/pages/api/robots.js
+++ website/src/pages/api/robots.js
@@ -1,11 +1,20 @@
+import { META } from 'lib/constants';
+import { trimTrailingSlash } from 'lib/utils';
+
 export default function handler(req, res) {
   if (
     process.env.VERCEL_ENV === 'production' &&
     !process.env.NOINDEX
   ) {
+    let sitemap = trimTrailingSlash(META.url) + '/sitemap_index.xml';
+
+    res.write(`Sitemap: ${sitemap}`);
+    res.write('\n');
     res.write('User-agent: *');
     res.write('\n');
     res.write('Allow: /');
+    res.write('\n');
+    res.write('Disallow: /api');
     res.send();
   } else {
     res.write('User-agent: *');
