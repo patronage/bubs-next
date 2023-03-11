022501f73cdf39ba70ff677e13000ff5bb4ad8c1
Cameron Corda <ccorda@gmail.com>
2023-03-11 12:55:31 -0800

    fix: upstream dashbaord customizations so title is preserved (#272)

--- wordpress/wp-content/themes/headless/setup/helpers/admin-env.php
+++ wordpress/wp-content/themes/headless/setup/helpers/admin-env.php
@@ -6,35 +6,34 @@
 $custom_scheme = '';
 $site_prefix = '';
 
-if ( WP_HOST === 'localhost' ) {
+if (WP_HOST === 'localhost') {
   // on Local
   $custom_scheme = 'ectoplasm';
   $site_prefix = 'LOCAL: ';
-} else if ( WP_HOST === 'staging' ) {
+} elseif (WP_HOST === 'staging') {
   $custom_scheme = 'sunrise';
   $site_prefix = 'STAGING: ';
 }
 
+if ($custom_scheme) {
+    function bubs_update_user_option_admin_color($color_scheme) {
+        global $custom_scheme;
+        $color_scheme = $custom_scheme;
+        return $color_scheme;
+    }
 
-if ( $custom_scheme ) {
-  function bubs_update_user_option_admin_color( $color_scheme ) {
-    global $custom_scheme;
-    $color_scheme = $custom_scheme;
-    return $color_scheme;
-  }
-
-  add_filter( 'get_user_option_admin_color', 'bubs_update_user_option_admin_color', 5 );
-  remove_action( 'admin_color_scheme_picker', 'admin_color_scheme_picker' );
+    add_filter('get_user_option_admin_color', 'bubs_update_user_option_admin_color', 5);
+    remove_action('admin_color_scheme_picker', 'admin_color_scheme_picker');
 }
 
-if ( $site_prefix ) {
-  function bubs_update_title_prefix() {
-    global $site_prefix;
-    global $site_name;
+if ($site_prefix) {
+    function bubs_update_title_prefix() {
+        global $site_prefix;
+        $site_name = get_option('blogname');
+        $site_name = str_replace(['LOCAL: ', 'STAGING: '], '', $site_name);
+        update_option('blogname', $site_prefix . $site_name);
+    }
 
-    update_option( 'blogname', $site_prefix . $site_name );;
-  }
-
-  add_action('admin_init', 'bubs_update_title_prefix');
+    add_action('admin_init', 'bubs_update_title_prefix');
 }
 ?>
