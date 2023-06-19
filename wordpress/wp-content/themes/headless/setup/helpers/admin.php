90106eefe222ce1dcb3fe2e6bddc364d6cb73b75
Cameron Corda <ccorda@gmail.com>
2023-06-19 10:44:32 -0700

    copy over prettier config from bubs-timber (#283)

--- wordpress/wp-content/themes/headless/setup/helpers/admin.php
+++ wordpress/wp-content/themes/headless/setup/helpers/admin.php
@@ -6,12 +6,12 @@
 
 // Go away "Comments" (SpamPots)
 function remove_comments_link() {
-    remove_menu_page( 'edit-comments.php' );
+    remove_menu_page('edit-comments.php');
 }
 
 function remove_comment_support() {
-    remove_post_type_support( 'post', 'comments' );
-    remove_post_type_support( 'page', 'comments' );
+    remove_post_type_support('post', 'comments');
+    remove_post_type_support('page', 'comments');
 }
 
 function remove_comments_admin() {
@@ -20,28 +20,27 @@ function remove_comments_admin() {
 }
 
 // Show pages list in DESC order by edit date, not alphabetical
-function set_post_order_in_admin( $wp_query ) {
+function set_post_order_in_admin($wp_query) {
     global $pagenow;
-    if ( is_admin() && 'edit.php' == $pagenow && !isset($_GET['orderby'])) {
-        $wp_query->set( 'orderby', 'modified' );
-        $wp_query->set( 'order', 'DSC' );
+    if (is_admin() && 'edit.php' == $pagenow && !isset($_GET['orderby'])) {
+        $wp_query->set('orderby', 'modified');
+        $wp_query->set('order', 'DSC');
     }
 }
-add_filter('pre_get_posts', 'set_post_order_in_admin' );
+add_filter('pre_get_posts', 'set_post_order_in_admin');
 
 //
 // Yoast
 //
 
 // Don't show WP SEO stuff in post list
-add_filter( 'wpseo_use_page_analysis', '__return_false' );
-
+add_filter('wpseo_use_page_analysis', '__return_false');
 
 // Lower WP SEO Priority so it's at the bottom of single posts:
 function lower_wpseo_priority() {
     return 'low';
 }
-add_filter( 'wpseo_metabox_prio', 'lower_wpseo_priority' );
+add_filter('wpseo_metabox_prio', 'lower_wpseo_priority');
 
 //
 // ACF
@@ -50,9 +49,8 @@ add_filter( 'wpseo_metabox_prio', 'lower_wpseo_priority' );
 // Hide ACF in production to ensure version controlled JSON is truth
 // http://awesomeacf.com/snippets/hide-the-acf-admin-menu-item-on-selected-sites/
 function awesome_acf_hide_acf_admin() {
-
     // check if the current env is dev
-    if ( defined('WP_ENV') && WP_ENV == "development" ) {
+    if (defined('WP_ENV') && WP_ENV == 'development') {
         // show the acf menu item
         return true;
     } else {
