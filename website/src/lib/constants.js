91b05dd00f37919b3f8f6c4c41c0ef3c7efb5446
Cameron Corda <ccorda@gmail.com>
2023-06-19 12:57:45 -0700

    minimal config in constants

--- website/src/lib/constants.js
+++ website/src/lib/constants.js
@@ -25,7 +25,7 @@ export const WORDPRESS_API_URL =
   (WORDPRESS_URL && WORDPRESS_URL + '/graphql') ||
   'https://bubsnext.wpengine.com/graphql';
 
-// TODO: Should these be camelCase?
+// TODO: Should these be camelCase?/
 export const CONFIG = {
   site_domain: 'bubs.patronage.org',
   wordpress_domain: WORDPRESS_DOMAIN,
@@ -37,15 +37,7 @@ export const CONFIG = {
 };
 
 // Theme defaults, these get merged with the project config
-// TODO: This is the full launch boat, need to trim this down to the minimum
 export const THEME = {
-  sectionBackgroundDefault: 'white',
-  bootstrapContentClasses: 'col-12 col-lg-10 col-xl-9 col-xxl-8',
-  spacer: 5,
-  paddingMobile: 2,
-  paddingDesktop: 4,
-  colorBody: '#fff',
-  internalDomains: [],
   meta: {
     icon32: '',
     iconApple: '/apple-touch-icon.png',
@@ -61,79 +53,5 @@ export const THEME = {
   integrations: {
     googleAnalyticsID: '',
     googleTagManagerID: '',
-  },
-  colors: {
-    primary: '#007bff',
-    secondary: '#6c757d',
-    light: '#F3F3F3',
-    medium: '#dee2e6',
-    dark: '#343a40',
-    text: '#212529',
-    body: '#ffffff',
-  },
-  fonts: {
-    heading: 'sans-serif',
-    body: 'sans-serif',
-    bodySize: '16px',
-    bodySizeMobile: '16px',
-    bodyLetterSpacing: 'initial',
-    bodyWeight: '400',
-    sourceGoogle: [],
-    sourceCustom: [],
-    h1max: '58px',
-    h1min: '28px',
-    h2max: '48px',
-    h2min: '25px',
-    h3max: '40px',
-    h3min: '22px',
-    h4max: '32px',
-    h4min: '20px',
-    h5max: '24px',
-    h5min: '18px',
-    h6max: '20px',
-    h6min: '16px',
-  },
-  // components
-  header: {
-    sectionBackground: 'white',
-    variant: 'default', // one of default, sticky, or seamless
-    logoText: '',
-    logoSrc: '',
-    logoWidth: 0,
-    logoHeight: 0,
-    showSocial: false,
-  },
-  footer: {
-    sectionBackground: 'dark',
-    logoText: '',
-    logoSrc: '',
-    logoWidth: 0,
-    logoHeight: 0,
-    disclaimerBorder: 0,
-    secondLogoText: '',
-    secondLogoSrc: '',
-    secondLogoWidth: 0,
-    secondLogoHeight: 0,
-    secondLogoLink: '',
-    secondLogoLinkTarget: '',
-  },
-  sections: {
-    headerAlignment: 'center', // default, one of 'center' or 'left'
-    headerWidth: 'content', // default, one of 'content' or 'dynamic'
-  },
-  hero: {
-    bgImageMask: 'default', // one of default, or none
-    buttonColorDark: 'primary',
-    buttonColorLight: 'white',
-    splitFullWidth: false,
-  },
-  featuredContent: {
-    loadMoreButtonSpacer: '0',
-    loadMoreText: 'See More',
-    readMoreText: 'Read More',
-    showDate: false,
-    showAuthor: false,
-    showTags: false,
-    showCategories: false,
   },
 };
